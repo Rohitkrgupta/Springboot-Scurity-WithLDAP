bdbfe0b15071f46651cee959a524be0ced4e6eea
hp <hp@DESKTOP-M1C4UQ9>
2020-07-14 16:44:45 +0530

    initial commit

---
+++ src/main/java/in/nit/SpringbootScurityAppWithLdapApplication.java
@@ -0,0 +1,13 @@
+package in.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootScurityAppWithLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootScurityAppWithLdapApplication.class, args);
+	}
+
+}
---
+++ src/main/java/in/nit/controller/ApplicationController.java
@@ -0,0 +1,18 @@
+package in.nit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class ApplicationController {
+	
+	
+	@GetMapping("/secure")
+	public String secureMethod()
+	{
+		return "This is secure method";
+	}
+
+}
